a50cb57ad2fdcb07a36bd9cb291bff530179b8e7
codefuchs <fady.kuzman@pm.me>
2023-01-26 20:57:07 +0100

    Add Tag and Operation annotation

--- src/main/java/dev/codefuchs/swaggerdemo/plant/PlantController.java
+++ src/main/java/dev/codefuchs/swaggerdemo/plant/PlantController.java
@@ -1,19 +1,27 @@
 package dev.codefuchs.swaggerdemo.plant;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
+
+@Tag(name = "Plant", description = "the Plant Api")
 @RestController
 @RequestMapping("/plant")
 public class PlantController {
-    @GetMapping
-    public ResponseEntity<Plant> getPlant() {
-        var plant = new Plant(1L, "plant", "this is a plant");
-        return ResponseEntity.of(Optional.of(plant));
+    @Autowired
+    private PlantService service;
+
+    @Operation(summary = "Fetch all plants", description = "fetches all plant entities and their data from data source")
+    @GetMapping("/all")
+    public ResponseEntity<List<Plant>> getAll() {
+        var plants = service.getAll();
+        return ResponseEntity.ok(plants);
     }
 }
